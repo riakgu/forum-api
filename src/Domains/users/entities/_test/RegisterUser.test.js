adcaf6fd97a93acf5bd3554bfbad5451a09a54b0
Rizky Akbar Gusnaen <rizkyakbargusnaen@gmail.com>
2025-03-23 16:54:16 +0700

    feat: implement authentication

--- src/Domains/users/entities/_test/RegisterUser.test.js
+++ src/Domains/users/entities/_test/RegisterUser.test.js
@@ -19,9 +19,11 @@ describe('a RegisterUser entities', () => {
             fullname: true,
             password: 'abc',
         };
+
         // Action and Assert
         expect(() => new RegisterUser(payload)).toThrowError('REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION');
     });
+
     it('should throw error when username contains more than 50 character', () => {
         // Arrange
         const payload = {
@@ -29,9 +31,11 @@ describe('a RegisterUser entities', () => {
             fullname: 'Dicoding Indonesia',
             password: 'abc',
         };
+
         // Action and Assert
         expect(() => new RegisterUser(payload)).toThrowError('REGISTER_USER.USERNAME_LIMIT_CHAR');
     });
+
     it('should throw error when username contains restricted character', () => {
         // Arrange
         const payload = {
@@ -39,9 +43,11 @@ describe('a RegisterUser entities', () => {
             fullname: 'dicoding',
             password: 'abc',
         };
+
         // Action and Assert
         expect(() => new RegisterUser(payload)).toThrowError('REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER');
     });
+
     it('should create registerUser object correctly', () => {
         // Arrange
         const payload = {
@@ -49,8 +55,10 @@ describe('a RegisterUser entities', () => {
             fullname: 'Dicoding Indonesia',
             password: 'abc',
         };
+
         // Action
         const { username, fullname, password } = new RegisterUser(payload);
+
         // Assert
         expect(username).toEqual(payload.username);
         expect(fullname).toEqual(payload.fullname);

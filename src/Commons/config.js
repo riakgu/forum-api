5e5d6d9672f41f62801350e45407ea375da7fc96
Rizky Akbar Gusnaen <rizkyakbargusnaen@gmail.com>
2025-03-23 17:04:56 +0700

    override .env with .test.env when NODE_ENV=test

--- src/Commons/config.js
+++ src/Commons/config.js
@@ -1,4 +1,15 @@
 /* istanbul ignore file */
+const dotenv = require('dotenv');
+const dotenvExpand = require('dotenv-expand');
+
+const envConfig = dotenv.config();
+dotenvExpand.expand(envConfig);
+
+if (process.env.NODE_ENV === 'test') {
+    const testEnvConfig = dotenv.config({ path: '.test.env', override: true });
+    dotenvExpand.expand(testEnvConfig);
+}
+
 const config = {
     app: {
         host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',

e8974677540b711b2dd1e888d5f95b76ffa4e94c
Rizky Akbar Gusnaen <rizkyakbargusnaen@gmail.com>
2025-03-23 16:55:18 +0700

    add jwt config

--- src/Commons/config.js
+++ src/Commons/config.js
@@ -12,5 +12,11 @@ const config = {
         password: process.env.PGPASSWORD,
         database: process.env.PGDATABASE,
     },
+    jwt: {
+        accessTokenKey: process.env.ACCESS_TOKEN_KEY,
+        refreshTokenKey: process.env.REFRESH_TOKEN_KEY,
+        accessTokenAge: process.env.ACCESS_TOKEN_AGE,
+    },
 };
+
 module.exports = config;

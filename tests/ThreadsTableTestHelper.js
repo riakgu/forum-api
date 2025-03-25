bae6a6afd19b97e725dfc0e29969ce7cd99be311
Rizky Akbar Gusnaen <rizkyakbargusnaen@gmail.com>
2025-03-25 12:49:38 +0700

    update addThread function in ThreadsTableTestHelper

--- tests/ThreadsTableTestHelper.js
+++ tests/ThreadsTableTestHelper.js
@@ -2,14 +2,13 @@
 const pool = require('../src/Infrastructures/database/postgres/pool');
 
 const ThreadsTableTestHelper = {
-    async addThread({id, owner, title, body}) {
+    async addThread(owner, {id, title, body}) {
         const query = {
             text: 'INSERT INTO threads (id, owner, title, body) VALUES ($1, $2, $3, $4) RETURNING id, title, owner',
             values: [id, owner, title, body],
         };
 
-        const result = await this._pool.query(query);
-        return result.rows;
+        await pool.query(query);
     },
 
     async findThreadById(threadId) {

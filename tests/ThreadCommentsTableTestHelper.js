907968660cfcfb7cf105fed633286dd7e50350d7
Rizky Akbar Gusnaen <rizkyakbargusnaen@gmail.com>
2025-03-25 12:48:26 +0700

    add findThreadCommentById function in ThreadCommentsTableTestHelper

--- tests/ThreadCommentsTableTestHelper.js
+++ tests/ThreadCommentsTableTestHelper.js
@@ -2,6 +2,16 @@
 const pool = require('../src/Infrastructures/database/postgres/pool');
 
 const ThreadCommentsTableTestHelper = {
+    async findThreadCommentById(commentId) {
+        const query = {
+            text: 'SELECT * FROM thread_comments WHERE id = $1',
+            values: [commentId],
+        };
+
+        const result = await pool.query(query);
+        return result.rows;
+    },
+
     async cleanTable(pool) {
         pool.query('TRUNCATE TABLE thread_comments CASCADE')
     }
